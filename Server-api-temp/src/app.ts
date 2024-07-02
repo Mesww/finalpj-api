1d9998f65523ff1c9503d38342dbce7ada8b7998
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-07-03 02:39:35 +0700

    websocket api

--- Server-api-temp/src/app.ts
+++ Server-api-temp/src/app.ts
@@ -3,15 +3,16 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { router } from "./routes/routes";
 import { authroute } from "./routes/authroutes";
-import { logger} from './middle/log';
-dotenv.config();
+import { logger } from './middle/log';
+import http from 'http';
+import WebSocket from 'ws';
+import axios from 'axios';
 
+dotenv.config();
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 const mongoDBUrl = process.env.MONGODB_URL;
-
 console.log(mongoDBUrl);
 
 if (!mongoDBUrl) {
@@ -29,6 +30,50 @@ app.use("/", router);
 app.use("/users", authroute);
 
 const PORT = process.env.PORT || 4444;
-app.listen(PORT, () => {
+
+
+
+
+// =========================================================
+// สร้าง HTTP server
+const server = http.createServer(app);
+// สร้าง WebSocket server
+const wss = new WebSocket.Server({ server });
+const BEARER_TOKEN = 'k3wbpy57L4pVQC';
+// กำหนด WebSocket logic
+wss.on('connection', (ws: WebSocket) => {
+  console.log('Client connected')
+
+  const fetchAndSendData = async () => {
+    try {
+      // เปลี่ยน URL นี้เป็น API ที่คุณต้องการ fetch
+      const response = await axios.get('https://www.ppgps171.com/mobile/api/mfutransit',{
+        headers: {
+          Authorization: `Bearer ${BEARER_TOKEN}`
+        }
+      });
+      ws.send(JSON.stringify(response.data));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  // เริ่ม polling ทันทีที่ client เชื่อมต่อ
+  fetchAndSendData();
+  const intervalId = setInterval(fetchAndSendData, 5000); // ทุก 5 วินาที
+
+  ws.on('message', (message: string) => {
+    console.log('Received message:', message);
+    // จัดการกับข้อความที่ได้รับจาก client ตามต้องการ
+  });
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clearInterval(intervalId);
+  });
+});
+// ======================================================
+
+
+// ใช้ server.listen แทน app.listen
+server.listen(PORT, () => {
   console.log(`Server is rocking at ${PORT}`);
 });
